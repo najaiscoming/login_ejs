a7264c39195271d9af5c7e93cccd5652b58b466b
najaiscoming <sarannoyprecha@gmail.com>
2021-12-07 11:34:06 +0700

    First try

---
+++ login.js
@@ -0,0 +1,7 @@
+//1.including package in our node.js
+var mysql = require("mysql");
+var express = require("express");
+var session = require("express-session");
+var bodypParser = require("body-parser");
+var path = require("path");
+
